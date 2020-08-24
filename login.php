e984d67f3ea5189a3b7e54d1cf45ef873f37199f
Gabriellrs <gabrielrsilva1609@gmail.com>
2020-08-24 11:54:39 -0300

    Telas de inicio, cadastro e login finalizadas.

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+		<title>TopCursos - A melhor plataforma de recomendação de cursos</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="assets/css/form.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<nav class="left">
+                    <a href="index.html" class="button alt">Voltar</a>
+				</nav>
+				<a href="index.html" class="logo">TopCursos</a>
+				<nav class="right">
+					<a href="#" class="button alt">Login</a>
+				</nav>
+            </header>
+
+        <!-- Form --> 
+        <div class="form">
+            <form>
+                <label>Email: </label><input type="text">
+                <label>Senha: </label><input type="password"><br>
+                <button class="btnFC">Entrar</button>
+            </form>
+        </div>           
+
+            <!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="actions">
+						<li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
+						<li><span class="icon fa-envelope"></span> <a href="#">emaildaequipe@gmail.com</a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Images <a href="https://unsplash.com">Unsplash</a>.
+				</div>
+			</footer>
+	</body>
+</html>
