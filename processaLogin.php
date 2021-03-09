0e1b9665128c52b869d63ad8abbb4aa29d412c78
Gabriel <gabrielrsilva1609@gmail.com>
2021-03-09 19:16:24 -0300

    tá feito chef

--- processaLogin.php
+++ processaLogin.php
@@ -1,22 +1,24 @@
 <?php 
 include_once("conexao.php");
+
 // session_start();
+
 if(isset($_POST['logar'])):
-$email=$_POST['email'];
-$senha=$_POST['senha'];
-$sql =  mysqli_query($conexao,"SELECT * FROM tbl_cadastro WHERE `email` = '$email' AND `senha` = '$senha' ");
-if(mysqli_num_rows($sql) > 0){
-$_SESSION['email'] = $email;
-$_SESSION['senha'] = $senha;  
-header('location:inicio.php');
-exit;
-}
-else{
-unset ($_SESSION['email']);
-unset ($_SESSION['senha']);
-header('location:index.html');
-exit;
-}
+    $email=$_POST['email'];
+    $senha=$_POST['senha'];
+    $sql =  mysqli_query($conexao,"SELECT * FROM tbl_cadastro WHERE `email` = '$email' AND `senha` = '$senha' ");
+        if(mysqli_num_rows($sql) > 0){
+            $_SESSION['email'] = $email;
+            $_SESSION['senha'] = $senha;  
+            header('location:inicio.php');
+        exit;
+    }
+    else{
+        unset ($_SESSION['email']);
+        unset ($_SESSION['senha']);
+        header('location:index.html');
+    exit;
+    }
 endif;
 mysqli_close($conexao);
 ?>
